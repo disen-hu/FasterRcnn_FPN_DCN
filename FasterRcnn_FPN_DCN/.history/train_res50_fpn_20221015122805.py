8696f7b9143d054ec8c5c421527aaf65d51e98ff
disen-hu <1515092118@qq.com>
2022-10-22 20:57:56 +0800

    add trained weights

--- FasterRcnn_FPN_DCN/.history/train_res50_fpn_20221015122805.py
+++ FasterRcnn_FPN_DCN/.history/train_res50_fpn_20221015122805.py
@@ -27,7 +27,7 @@ def create_model(num_classes, load_pretrain_weights=True):
     if load_pretrain_weights:
         # 载入预训练模型权重
         # https://download.pytorch.org/models/fasterrcnn_resnet50_fpn_coco-258fb6c6.pth
-        weights_dict = torch.load("./backbone/fasterrcnn_resnet50_fpn_coco.pth", map_location='cpu')
+        weights_dict = torch.load("./backbone/fasterrcnn_resnet50_fpn_coco-258fb6c6.pth", map_location='cpu')
         missing_keys, unexpected_keys = model.load_state_dict(weights_dict, strict=False)
         if len(missing_keys) != 0 or len(unexpected_keys) != 0:
             print("missing_keys: ", missing_keys)
